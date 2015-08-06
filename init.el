c1554e4ee753745a2d4b5bb712e9293f0e9504a1
WChurchill <winston@Toshiba-Arch.localdomain>
2015-08-06 17:36:02 -0500

    Put org.el back into the org hook

--- init.el
+++ init.el
@@ -2,3 +2,9 @@
 (load "/home/winston/.emacs.d/noexternals.el")
 (load "/home/winston/.emacs.d/keybindings.el")
 (load "/home/winston/.emacs.d/loadpackages.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote "~/org/cal.org")))
--- loadpackages.el
+++ loadpackages.el
@@ -29,7 +29,12 @@
 ;;; ORG-MODE
 (require 'org)
 (add-hook 'org-mode-hook
-	  (load "org.el"))
+	  (lambda ()
+	    (local-set-key (kbd "C-c a") 'org-agenda)
+	    (local-set-key (kbd "C-M-f") 'org-forward-heading-same-level)
+	    (local-set-key (kbd "C-M-b") 'org-backward-heading-same-level)
+	    (local-set-key (kbd "C-c C-f") 'org-down-element)
+	    (local-set-key (kbd "C-c C-b") 'org-up-element)))
 
 ;;; MULTI-TERM
 (require 'multi-term)
