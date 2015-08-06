b6e22f8dcf0141c97b5b6dc4c0943674ac3ed627
WChurchill <winston@Toshiba-Arch.localdomain>
2015-08-06 14:40:32 -0500

    Loaded keybindings.el and fixed edit-emacs

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
 (load "/home/winston/.emacs.d/themes/euphoria-theme.el")
 (load "/home/winston/.emacs.d/noexternals.el")
+(load "/home/winston/.emacs.d/keybindings.el")
 (load "/home/winston/.emacs.d/loadpackages.el")
--- noexternals.el
+++ noexternals.el
@@ -1,10 +1,15 @@
 ;; Startup workspace
 (defun find-my-files ()
-  (find-file "/home/winston/org/main.org")
+  (find-file "~/org/main.org")
   (split-window-horizontally)
-  (find-file "/home/winston/lisp/quicklisp/local-projects/machine-learning/linreg.lisp"))
+  (find-file "~/lisp/quicklisp/local-projects/machine-learning/linreg.lisp"))
 
 (find-my-files)
+
+;; Easier editing of .emacs.d/
+(defun emacs-dir ()
+  (interactive)
+  (find-file "~/.emacs.d"))
 
 ;; Font
 (setq line-spacing 0)
@@ -26,14 +31,6 @@
 
 ;; Don’t defer screen updates when performing operations
 (setq redisplay-dont-pause t)
-
-;; Joins Lines into one (from the bottom up)
-(global-set-key (kbd "M-j") '(lambda () (interactive) (join-line -1)))
-;;from top down
-(global-set-key (kbd "M-6") 'join-line)
-
-;; Kill line from the left
-(global-set-key (kbd "<s-backspace>") '(lambda () (interactive) (kill-line 0)))
 
 ;; Start show-paren-mode
 (show-paren-mode 1)
@@ -67,26 +64,6 @@ buffer is not visiting a file."
 ;; Cursor Blinking
 (setq blink-cursor-blinks 9999)
 
-;; Backward-kill-sexp
-(global-set-key (kbd "C-M-<backspace>") 'backward-kill-sexp)
-
-;; Replace List-buffers with Ibuffer
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;;Quicker Keybinding Writing
-(defun new-keybinding ()
-  "Inserts the string \"(global-set-key (kbd\"\") ') and places point in between the quotes."
-  (interactive)
-  (insert "(global-set-key (kbd \"\") ')")
-  (backward-char 5))
-
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda () (local-set-key (kbd "C-c C-k") 'new-keybinding)))
-
-;; Copy and Kill region
-(global-set-key (kbd "C-w") 'copy-region-as-kill)
-(global-set-key (kbd "C-x w") 'kill-region)
-
 ;; Remove scrollbars, menubars, startup screen, and toolbar
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
@@ -99,29 +76,3 @@ buffer is not visiting a file."
 ;(global-set-key (kbd "C-c C-p") 'windmove-up)
 ;(global-set-key (kbd "C-c C-f") 'windmove-right)
 
-;; Better Window management
-(global-set-key (kbd "M-s-b") 'shrink-window-horizontally)
-(global-set-key (kbd "M-s-f") 'enlarge-window-horizontally)
-(global-set-key (kbd "M-s-n") 'shrink-window)
-(global-set-key (kbd "M-s-p") 'enlarge-window)
-
-(global-set-key (kbd "M-0") 'delete-window)
-(global-set-key (kbd "M-1") 'delete-other-windows)
-(global-set-key (kbd "M-2") 'split-window-horizontally)
-(global-set-key (kbd "M-3") 'split-window-vertically)
-(global-set-key (kbd "M-4") 'kill-buffer-and-window)
-(global-set-key (kbd "M-=") 'balance-windows)
-
-;; Killing buffers and Windows
-(defun kill-current-buffer ()
-  "Kills the current buffer."
-  (interactive)
-  (kill-buffer (buffer-name)))
-
-(global-set-key (kbd "M--") 'kill-current-buffer)
-
-;; Horizontal Scrolling
-(put 'scroll-left 'disabled nil)
-(put 'scroll-right 'disabled nil)
-(global-set-key (kbd "C-<") 'scroll-left)
-(global-set-key (kbd "C->") 'scroll-right)
