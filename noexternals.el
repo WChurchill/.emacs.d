4985dcc32cb8c541fdbdd8255b1e166e2c234f8a
Winston Carlile <winston.carlile@gmail.com>
2016-01-02 13:39:13 -0600

    Removed unecessary empty lines

--- noexternals.el
+++ noexternals.el
@@ -97,11 +97,6 @@ With a prefix ARG prompt for a file to visit.
 Will also prompt for a file to visit if current
 buffer is not visiting a file."
   (interactive "P")
-  
-  
-  
-  
-  
   (if (or arg (not buffer-file-name))
       (find-file (concat "/sudo:root@localhost:"
                          (ido-read-file-name "Find file(as root): ")))
