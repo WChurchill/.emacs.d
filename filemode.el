c3a000f2b1f3eecd390ebd0f7cf5130e197df183
Winston Carlile <winston.carlile@gmail.com>
2016-10-18 10:29:42 -0500

    Don't use octave mode on .m files

--- filemode.el
+++ filemode.el
@@ -1,7 +1,7 @@
 ;;; filemode.el
 
 ;; set .m file extension to invoke octave-mode
-(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
+;;(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
 
 ;; file extensions for the Gazebo robot simulator, which uses xml format 
 (add-to-list 'auto-mode-alist '("\\.world\\'" . nxml-mode))
