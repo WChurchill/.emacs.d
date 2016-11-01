e6c465e595aed7782775b6e15bd3401722518d7b
Winston Carlile <winston.carlile@gmail.com>
2016-10-31 21:21:58 -0500

    Added default modes for .cu and .m extensions

--- filemode.el
+++ filemode.el
@@ -1,8 +1,11 @@
 ;;; filemode.el
 
-;; set .m file extension to invoke octave-mode
-;;(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
+;; set .m file extension to invoke matlab-mode
+(add-to-list 'auto-mode-alist '("\\.m\\'" . matlab-mode))
 
 ;; file extensions for the Gazebo robot simulator, which uses xml format 
 (add-to-list 'auto-mode-alist '("\\.world\\'" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.launch\\'" . nxml-mode))
+
+;; Open ".cu" cuda files in c++ mode
+(add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode))
