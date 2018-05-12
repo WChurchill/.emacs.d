f56218346d85e0862b0662f6b938bb3e53081acc
Winston Carlile <winston.carlile@gmail.com>
2018-05-11 22:21:17 -0700

    Load melpa repository

--- init.el
+++ init.el
@@ -8,8 +8,9 @@
   (setq gc-cons-threshold temp-cons-threshold)
 
   ;; must call before package configuration
-  (package-initialize)
+  (require 'package)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  (package-initialize)
   
   (load "/home/winston/.emacs.d/noexternals.el")
   (load "/home/winston/.emacs.d/keybindings.el")
@@ -30,7 +31,7 @@
  '(org-export-backends (quote (ascii beamer html icalendar latex md odt)))
  '(package-selected-packages
    (quote
-	(magit which-key flycheck-ledger ledger-mode pdf-tools interleave ecb jedi helm-company elpy helm-gtags helm-bibtex scad-mode helm-cscope py-autopep8 py-yapf helm-flycheck electric-spacing php-mode csharp-mode typescript-mode exec-path-from-shell cmake-mode swiper smart-mode-line-powerline-theme python-mode paredit multi-web-mode multiple-cursors multi-term matlab-mode lua-mode highlight-quoted highlight-numbers helm-projectile ggtags emacs-eclim company-cmake company-c-headers company-auctex cmake-font-lock autopair ace-window ac-js2))))
+	(yasnippet-snippets undo-tree importmagic magit which-key flycheck-ledger ledger-mode pdf-tools interleave ecb jedi helm-company elpy helm-gtags helm-bibtex scad-mode helm-cscope py-autopep8 py-yapf helm-flycheck electric-spacing php-mode csharp-mode typescript-mode exec-path-from-shell cmake-mode swiper smart-mode-line-powerline-theme python-mode paredit multi-web-mode multiple-cursors multi-term matlab-mode lua-mode highlight-quoted highlight-numbers helm-projectile ggtags emacs-eclim company-cmake company-c-headers company-auctex cmake-font-lock autopair ace-window ac-js2))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
